c3fbec4f140c26e0c32ab54e980fb85cb592983d
rbexcfc <richie.bexhell@caternet.co.uk>
2017-06-12 13:01:27 +0100

    Initial Commit

---
+++ SpecflowSeleniumPOM/Pages/HomePage.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using NUnit.Framework;
+
+namespace SpecflowSeleniumPOM.Pages
+{
+    public class HomePage
+    {
+        private IWebDriver driver;
+
+        [FindsBy(How = How.Id, Using = "twotabsearchtextbox")]
+        public IWebElement searchBox;
+
+        [FindsBy(How = How.ClassName, Using = "nav-input")]
+        public IWebElement searchButton;
+
+        [FindsBy(How = How.LinkText, Using = "adidas Men's Chelsea Fc 2016 Home Jersey")]
+        public IWebElement chelseaShirt;
+
+        [FindsBy(How = How.Id, Using = "productTitle")]
+        public IWebElement productTitle;
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);     
+        }
+        public void searchProduct()
+        {
+            searchBox.SendKeys("Chelsea shirt");
+
+            searchButton.Click();
+        }
+
+        public void selectProduct()
+        {
+            chelseaShirt.Click();
+        }
+
+        public void checkProduct()
+        {
+            string title = productTitle.Text;
+
+            Assert.That(title.Contains("adidas Men's Chelsea Fc 2016 Home Jersey"));
+        }
+
+    }
+}
---
+++ SpecflowSeleniumPOM/Tests(Standard)/SearchProjectTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SpecflowSeleniumPOM.Pages;
+
+namespace SpecflowSeleniumPOM.Tests
+{
+    class SearchProjectTest
+    {
+        [Test]
+        public void test()
+        {
+            IWebDriver driver = new ChromeDriver();
+
+            driver.Url = "https://www.amazon.co.uk";
+
+            driver.Manage().Window.Maximize();
+
+            var homepage = new HomePage(driver);
+
+            homepage.searchProduct();
+
+            homepage.selectProduct();
+
+            homepage.checkProduct();
+
+            driver.Close();
+        }
+    }
+}
